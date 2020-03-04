868b2a6923682ef9d65df135022f57bf744df081
Ruben <rubendiazee@gmail.com>
2020-03-04 08:58:00 +0100

    tabla entrenador modelada en el proyecto, añadido script en resources

---
+++ src/main/java/talent/campus/examenRubenDiaz/model/Entrenador.java
@@ -0,0 +1,63 @@
+package talent.campus.examenRubenDiaz.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "entrenador")
+public class Entrenador {
+
+	public Entrenador() {
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_entrenador", updatable = false, nullable = false)
+	private Integer idEntrenador;
+
+	@Column(name = "nombre")
+	private String nombre;
+
+	@Column(name = "apellidos")
+	private String apellidos;
+
+	@Column(name = "edad")
+	private Integer edad;
+
+	public Integer getIdEntrenador() {
+		return this.idEntrenador;
+	}
+
+	public void setIdEntrenador(Integer idEntrenador) {
+		this.idEntrenador = idEntrenador;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return this.apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Integer getEdad() {
+		return this.edad;
+	}
+
+	public void setEdad(Integer edad) {
+		this.edad = edad;
+	}
+
+}
