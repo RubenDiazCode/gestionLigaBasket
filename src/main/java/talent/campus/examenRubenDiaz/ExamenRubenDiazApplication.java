485346f0500dc0d4bcfe5ebd6f9707ab5ae3989f
Ruben <rubendiazee@gmail.com>
2020-03-04 08:41:51 +0100

    primer commit

---
+++ src/main/java/talent/campus/examenRubenDiaz/ExamenRubenDiazApplication.java
@@ -0,0 +1,13 @@
+package talent.campus.examenRubenDiaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenRubenDiazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenRubenDiazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/talent/campus/examenRubenDiaz/ExamenRubenDiazApplicationTests.java
@@ -0,0 +1,13 @@
+package talent.campus.examenRubenDiaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenRubenDiazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
