ca7b44b27f140afbfc4095a35233146feb78bbd5
Ruben <rubendiazee@gmail.com>
2020-03-05 14:50:17 +0100

    test de entrenadorResource completados

--- src/test/java/talent/campus/examenRubenDiaz/web/rest/EntrenadorResourceTest.java
+++ src/test/java/talent/campus/examenRubenDiaz/web/rest/EntrenadorResourceTest.java
@@ -3,6 +3,7 @@ package talent.campus.examenRubenDiaz.web.rest;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.junit.Assert;
@@ -24,6 +25,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import talent.campus.examenRubenDiaz.AssertUtils;
 import talent.campus.examenRubenDiaz.dto.EntrenadorPayload;
+import talent.campus.examenRubenDiaz.model.Entrenador;
 import talent.campus.examenRubenDiaz.repository.EntrenadorRepository;
 
 @RunWith(SpringRunner.class)
@@ -46,18 +48,102 @@ public class EntrenadorResourceTest {
 		ObjectMapper objectMapper = new ObjectMapper();
 		return objectMapper.readValue(json, clazz);
 	}
-	
+
 	@Test
 	public void testFindAll() throws Exception {
-        MvcResult mvcResult = this.mvc.perform(MockMvcRequestBuilders.get("/api/entrenador/")
-                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
-        Assert.assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
-        String content = mvcResult.getResponse().getContentAsString();
-        System.out.println(content);
-        
-        EntrenadorPayload[] response = this.mapFromJson(content, EntrenadorPayload[].class);
-        List<Integer> real=Arrays.stream(response).map(entrenadorPayload -> entrenadorPayload.getIdEntrenador()).collect(Collectors.toList());
-        AssertUtils.assertEquals(Arrays.asList(1,2), real);
-        
+		MvcResult mvcResult = this.mvc
+				.perform(MockMvcRequestBuilders.get("/api/entrenador/").accept(MediaType.APPLICATION_JSON_VALUE))
+				.andReturn();
+		Assert.assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
+		String content = mvcResult.getResponse().getContentAsString();
+		System.out.println(content);
+
+		EntrenadorPayload[] response = this.mapFromJson(content, EntrenadorPayload[].class);
+		List<Integer> real = Arrays.stream(response).map(entrenadorPayload -> entrenadorPayload.getIdEntrenador())
+				.collect(Collectors.toList());
+		AssertUtils.assertEquals(Arrays.asList(1, 2), real);
+
 	}
+
+	@Test
+	public void testFindById() throws Exception {
+		MvcResult mvcResult = this.mvc.perform(
+				MockMvcRequestBuilders.get("/api/entrenador/id/{id}/", 1).accept(MediaType.APPLICATION_JSON_VALUE))
+				.andReturn();
+		Assert.assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
+		String content = mvcResult.getResponse().getContentAsString();
+		EntrenadorPayload response = this.mapFromJson(content, EntrenadorPayload.class);
+
+		Assert.assertEquals(Integer.valueOf(1), response.getIdEntrenador());
+		Assert.assertEquals("test", response.getNombre());
+		Assert.assertEquals("entrenador1", response.getApellidos());
+		Assert.assertEquals(Integer.valueOf(77), response.getEdad());
+	}
+
+	@Test
+	public void testCreate() throws Exception {
+		EntrenadorPayload request = new EntrenadorPayload();
+		request.setNombre("Gregg");
+		request.setApellidos("Popovich");
+		request.setEdad(78);
+
+		MvcResult mvcResult = this.mvc.perform(MockMvcRequestBuilders.post("/api/entrenador/")
+				.contentType(MediaType.APPLICATION_JSON_VALUE).content(this.mapToJson(request))).andReturn();
+		Assert.assertEquals(HttpStatus.CREATED.value(), mvcResult.getResponse().getStatus());
+		EntrenadorPayload response = this.mapFromJson(mvcResult.getResponse().getContentAsString(),
+				EntrenadorPayload.class);
+
+		Assert.assertEquals("Gregg", response.getNombre());
+		Assert.assertEquals("Popovich", response.getApellidos());
+		Assert.assertEquals(Integer.valueOf(78), response.getEdad());
+
+		Optional<Entrenador> entrenadorOption = this.entrenadorRepository.findById(response.getIdEntrenador());
+		Assert.assertTrue(entrenadorOption.isPresent());
+
+		Entrenador entrenador = entrenadorOption.get();
+		Assert.assertEquals("Gregg", entrenador.getNombre());
+		Assert.assertEquals("Popovich", entrenador.getApellidos());
+		Assert.assertEquals(Integer.valueOf(78), entrenador.getEdad());
+
+	}
+
+	@Test
+	public void testDelete() throws Exception {
+		MvcResult mvcResult = this.mvc.perform(
+				MockMvcRequestBuilders.delete("/api/entrenador/{id}/", 1).accept(MediaType.APPLICATION_JSON_VALUE))
+				.andReturn();
+		Assert.assertEquals(HttpStatus.NO_CONTENT.value(), mvcResult.getResponse().getStatus());
+		Optional<Entrenador> entrenadorOption = this.entrenadorRepository.findById(1);
+		Assert.assertFalse(entrenadorOption.isPresent());
+
+	}
+
+	@Test
+	public void testUpdate() throws Exception {
+		EntrenadorPayload request = new EntrenadorPayload();
+		request.setNombre("Gregg");
+		request.setApellidos("Popovich");
+		request.setEdad(78);
+
+		MvcResult mvcResult = this.mvc.perform(MockMvcRequestBuilders.put("/api/entrenador/1/")
+				.contentType(MediaType.APPLICATION_JSON_VALUE).content(this.mapToJson(request))).andReturn();
+		Assert.assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus());
+		
+		EntrenadorPayload response = this.mapFromJson(mvcResult.getResponse().getContentAsString(),
+				EntrenadorPayload.class);
+		Assert.assertEquals(Integer.valueOf(1), response.getIdEntrenador());
+		Assert.assertEquals("Gregg", response.getNombre());
+		Assert.assertEquals("Popovich", response.getApellidos());
+		Assert.assertEquals(Integer.valueOf(78), response.getEdad());
+		
+		Optional<Entrenador> entrenadorOption = this.entrenadorRepository.findById(response.getIdEntrenador());
+		Assert.assertTrue(entrenadorOption.isPresent());
+
+		Entrenador entrenador = entrenadorOption.get();
+		Assert.assertEquals("Gregg", entrenador.getNombre());
+		Assert.assertEquals("Popovich", entrenador.getApellidos());
+		Assert.assertEquals(Integer.valueOf(78), entrenador.getEdad());
+		
+	}
+
 }
