7e600e3f552dabc5bc2aec4ba8212ebc28bc4f16
Ruben <rubendiazee@gmail.com>
2020-03-06 11:50:01 +0100

    script de bbdd añadido a resources

---
+++ src/main/resources/scriptBD.sql
@@ -0,0 +1,140 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: examentalent
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `entrenador`
+--
+CREATE SCHEMA `examentalent` ;
+DROP TABLE IF EXISTS `entrenador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `entrenador` (
+  `id_entrenador` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) NOT NULL,
+  `apellidos` varchar(20) DEFAULT NULL,
+  `edad` int NOT NULL,
+  PRIMARY KEY (`id_entrenador`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entrenador`
+--
+
+LOCK TABLES `entrenador` WRITE;
+/*!40000 ALTER TABLE `entrenador` DISABLE KEYS */;
+INSERT INTO `entrenador` VALUES (1,'frank','vogel',45);
+/*!40000 ALTER TABLE `entrenador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `equipo`
+--
+
+DROP TABLE IF EXISTS `equipo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `equipo` (
+  `id_equipo` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) NOT NULL,
+  `anyo_fundacion` int DEFAULT NULL,
+  `entrenador_id_entrenador` int DEFAULT NULL,
+  PRIMARY KEY (`id_equipo`),
+  UNIQUE KEY `id_equipo_UNIQUE` (`id_equipo`),
+  UNIQUE KEY `nombre_UNIQUE` (`nombre`),
+  KEY `fk_equipo_entrenador1_idx` (`entrenador_id_entrenador`),
+  CONSTRAINT `fk_equipo_entrenador1` FOREIGN KEY (`entrenador_id_entrenador`) REFERENCES `entrenador` (`id_entrenador`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `equipo`
+--
+
+LOCK TABLES `equipo` WRITE;
+/*!40000 ALTER TABLE `equipo` DISABLE KEYS */;
+INSERT INTO `equipo` VALUES (1,'Philadelphia 76ers',1946,3),(2,'atlanta hawks',1950,7);
+/*!40000 ALTER TABLE `equipo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jugador`
+--
+
+DROP TABLE IF EXISTS `jugador`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `jugador` (
+  `id_jugador` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) NOT NULL,
+  `apellidos` varchar(20) DEFAULT NULL,
+  `edad` int NOT NULL,
+  `equipo_id_equipo` int DEFAULT NULL,
+  PRIMARY KEY (`id_jugador`),
+  KEY `fk_jugador_equipo_idx` (`equipo_id_equipo`),
+  CONSTRAINT `fk_jugador_equipo` FOREIGN KEY (`equipo_id_equipo`) REFERENCES `equipo` (`id_equipo`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jugador`
+--
+
+LOCK TABLES `jugador` WRITE;
+/*!40000 ALTER TABLE `jugador` DISABLE KEYS */;
+INSERT INTO `jugador` VALUES (1,'Joel','Embiid',25,1),(2,'Vince','Carter',41,6);
+/*!40000 ALTER TABLE `jugador` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `partido`
+--
+
+DROP TABLE IF EXISTS `partido`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `partido` (
+  `id_partido` int NOT NULL AUTO_INCREMENT,
+  `id_equipo_local` int NOT NULL,
+  `id_equipo_visitante` int NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `puntuacion_equipo_local` int DEFAULT NULL,
+  `puntuacion_equipo_visitante` int DEFAULT NULL,
+  PRIMARY KEY (`id_partido`,`id_equipo_local`,`id_equipo_visitante`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `partido`
+--
+
+LOCK TABLES `partido` WRITE;
+/*!40000 ALTER TABLE `partido` DISABLE KEYS */;
+INSERT INTO `partido` VALUES (1,1,2,'2019-06-20',80,69);
+/*!40000 ALTER TABLE `partido` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-03-06 11:42:32
